ddd8a1f2c5bff523ef5fda670db2cafb3c25c212
Fauzan <fauzanlubis23@gmail.com>
2020-04-05 18:30:18 +0800

    styling

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -4,8 +4,8 @@ export default function Userlist(props) {
   return (
     <div className="mt-10 flex items-center justify-center">
       <div className="container my-20">
-        <div className="w-full rounded-lg overflow-hidden sm:shadow-lg lg:bg-gray-400">
-          <p class="m-4 font-sans sm:text-xl lg:text-2xl text-white lg:text-gray-800 text-center font-extrabold tracking-widest">
+        <div className="w-full rounded-lg rounded-b-none overflow-hidden sm:shadow-lg lg:bg-gray-700">
+          <p class="m-4 font-sans text-2xl text-white text-center font-extrabold tracking-widest">
             LIST REGISTERED USERS
           </p>
         </div>
@@ -13,10 +13,10 @@ export default function Userlist(props) {
           return (
             <ul className=" bg-teal-100 text-center shadow-xl rounded">
               <li
-                className="shadow-xl rounded mt-1 py-3 px-12 border border-gray-400 bg-gray-700"
+                className="shadow-xl rounded py-3 px-12 border border-gray-700  lg:bg-gray-400"
                 key={idx}
               >
-                <p class="ml-4 font-sans text-xl text-white text-center font-extrabold tracking-widest">
+                <p class="ml-4 font-sans text-xl text-gray-700 text-center font-extrabold tracking-widest">
                   {data.name}
                 </p>
               </li>
